1b83325d6b09fde6978d66c2d0db9419c09bbea7
majkelw <michalwik@vp.pl>
2021-11-25 13:37:58 +0100

    Added dummy endpoints, to produces specified output format

--- src/main/java/com/ppkwu/lab4/controller/DataConverterController.java
+++ src/main/java/com/ppkwu/lab4/controller/DataConverterController.java
@@ -1,5 +1,6 @@
 package com.ppkwu.lab4.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DataConverterController {
 
-    @PostMapping("api/converter/{from}/{to}")
-    public String convertDataFromTo(@PathVariable("from") String from, @PathVariable("to") String to) {
+    @PostMapping(value = "api/converter/{from}/json", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public String convertDataToJson(@PathVariable("from") String from) {
         return "test";
+    }
+
+    @PostMapping(value = "api/converter/{from}/xml", produces = {MediaType.APPLICATION_XML_VALUE})
+    public String convertDataToXml(@PathVariable("from") String from) {
+        return "test2";
+    }
+
+    @PostMapping(value = "api/converter/{from}/csv", produces = {MediaType.TEXT_PLAIN_VALUE})
+    public String convertDataToCsv(@PathVariable("from") String from) {
+        return "test3";
+    }
+
+    @PostMapping(value = "api/converter/{from}/txt", produces = {MediaType.TEXT_PLAIN_VALUE})
+    public String convertDataToTxt(@PathVariable("from") String from) {
+        return "test4";
     }
 }
