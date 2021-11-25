ad9698730dd7e259714d1d8c6bb5fb992a8376dc
majkelw <michalwik@vp.pl>
2021-11-25 13:17:48 +0100

    Init project, changed default port

---
+++ src/main/java/com/ppkwu/lab4/PpkwuLab4Application.java
@@ -0,0 +1,13 @@
+package com.ppkwu.lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpkwuLab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpkwuLab4Application.class, args);
+	}
+
+}
