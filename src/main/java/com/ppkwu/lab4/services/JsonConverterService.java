1982214bd6952743ac63060787b8a0b6aa4091a7
majkelw <michalwik@vp.pl>
2021-11-25 13:38:20 +0100

    Added json converter

---
+++ src/main/java/com/ppkwu/lab4/services/JsonConverterService.java
@@ -0,0 +1,35 @@
+package com.ppkwu.lab4.services;
+
+import org.json.CDL;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.XML;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonConverterService {
+
+    public String toXml(String json) {
+        return "<data>" + XML.toString(new JSONObject(json)) + "</data>";
+    }
+
+    public String toCsv(String json) {
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.put(new JSONObject(json));
+        return CDL.toString(jsonArray);
+    }
+
+    public String toTxt(String json) {
+        JSONObject jsonObject = new JSONObject(json);
+        StringBuilder txt = new StringBuilder();
+        jsonObject.keySet().forEach(key -> {
+            Object value = jsonObject.get(key);
+            txt.append(key).append(": ").append(value).append("\n");
+        });
+        if (txt.length() > 0)
+            txt.deleteCharAt(txt.length() - 1);
+        return txt.toString();
+    }
+
+}
+
